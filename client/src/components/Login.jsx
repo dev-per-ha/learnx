5b8addd30538b347c52fe0f917545093fec2a29f
Dawit Hailemariyam <devper95@gmail.com>
2025-08-22 15:58:34 +0300

    intial commit

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     setMessage({ type: '', text: '' });
 
     try {
-      const res = await fetch('http://localhost:5000/api/login', {
+      const res = await fetch('https://learnxbackend.onrender.com/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
