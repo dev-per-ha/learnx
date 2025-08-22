e2085a3a5ad774d541ab193d60f49a5d4e7813d6
Dawit Hailemariyam <devper95@gmail.com>
2025-08-22 16:05:55 +0300

    intial commit

--- admin/src/pages/AdminLogin.jsx
+++ admin/src/pages/AdminLogin.jsx
@@ -18,7 +18,7 @@ const AdminLogin = () => {
     setMessage({ type: '', text: '' });
 
     try {
-      const res = await fetch('http://localhost:5000/api/admin/login', {
+      const res = await fetch('https://learnxbackend.onrender.com/api/admin/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
